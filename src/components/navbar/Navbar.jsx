969dd0fa12a67791d697adba903bf9c72827f442
Cass <cassianoleaoramos@gmail.com>
2023-11-03 17:49:58 -0300

    styling the navigation bar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
+import styles from "./navbar.module.css"
 
 const links = [{
     id: "1", title: "Home", url: "/",
@@ -22,14 +25,21 @@ const links = [{
 
 const Navbar = () => {
     return (
-        <div>
-            <Link href="/"></Link>
-            <div>
+        <div className={styles.container}>
+            <Link href="/" className={styles.logo}>
+                Cass
+            </Link>
+            <div className={styles.links}>
                 {links.map((link) => (
-                    <Link key={link.id} href={link.url}>
+                    <Link key={link.id} href={link.url} className={styles.link}>
                         {link.title}
                     </Link>
                 ))}
+                <button className={styles.logout} onClick={() => {
+                    console.log("logged out!")
+                }}>
+                    Logout
+                </button>
             </div>
         </div>
     )
