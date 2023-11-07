26ec4d2807863673a4b54a862164920785b29d4e
Cass <cassianoleaoramos@gmail.com>
2023-11-06 22:56:54 -0300

    fetching data from local JSON file

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,15 +1,27 @@
 import Image from 'next/image';
 import React from 'react'
 import styles from "./page.module.css"
+import { notFound } from 'next/navigation';
 
-const BlogPost = () => {
+async function getData({ id }) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { cache: "no-store" })
+
+    if (!res.ok) {
+        return notFound
+    }
+
+    return res.json()
+}
+
+const BlogPost = async ({ params }) => {
+    const data = await getData(params.id)
     return (
         <div className={styles.container}>
             <div className={styles.top}>
                 <div className={styles.info}>
-                    <h1 className={styles.title}>Title</h1>
+                    <h1 className={styles.title}>{data.title}</h1>
                     <p className={styles.desc}>
-                        Desc
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed quo minima, quas quibusdam distinctio dolores ipsum eos ducimus sit voluptatibus. Quo qui id aspernatur quia molestiae quaerat possimus fugiat neque.
                     </p>
                     <div className={styles.author}>
                         <Image
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,27 +3,41 @@ import styles from "./page.module.css"
 import Link from 'next/link'
 import Image from 'next/image'
 
+async function getData() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", { cache: "no-store" })
 
-const Blog = () => {
+    if (!res.ok) {
+        throw new Error("Failed to fetch data")
+    }
+
+    return res.json()
+}
+
+const Blog = async () => {
+    const data = await getData()
     return (
         <div className={styles.mainContainer}>
-            <Link href="/blog/testId" className={styles.container}>
-                <div className={styles.imageContainer}>
-                    <Image
-                        src=""
-                        alt=""
-                        width={400}
-                        height={250}
-                        className={styles.image}
-                    />
-                </div>
-                <div className={styles.content}>
-                    <h1 className={styles.title}></h1>
-                    <p className={styles.desc}></p>
-                </div>
-            </Link>
+            {data.map((item) => (
+                <Link href="/blog/testId" className={styles.container} key={item.id}>
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src=""
+                            alt=""
+                            width={400}
+                            height={250}
+                            className={styles.image}
+                        />
+                    </div>
+                    <div className={styles.content}>
+                        <h1 className={styles.title}></h1>
+                        <p className={styles.desc}></p>
+                    </div>
+                </Link>
+
+            ))}
+
         </div>
-    )
+    );
 }
 
 export default Blog
