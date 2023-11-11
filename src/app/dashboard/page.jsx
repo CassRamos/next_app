c6ca4e9d9d0a395ac500895b348b3632dd745df1
Cass <cassianoleaoramos@gmail.com>
2023-11-11 16:55:25 -0300

    Initialize auth with Google authentication

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,8 +2,13 @@
 import React from 'react'
 import styles from "./page.module.css"
 import useSWR from 'swr'
+import { useSession } from 'next-auth/react'
 
 const Dashboard = () => {
+
+    const session = useSession()
+   
+
     const fetcher = (...args) => fetch(...args).then(res => res.json())
 
     const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher)
@@ -12,4 +17,3 @@ const Dashboard = () => {
     )
 }
 
-export default Dashboard
