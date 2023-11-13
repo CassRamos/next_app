52f704e341b19f1418d21bad71cd622a7326ec29
Cass <cassianoleaoramos@gmail.com>
2023-11-12 23:13:05 -0300

    Finalize the dashboard page

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -1,6 +1,6 @@
-import Post from "@/models/Post";
-import connect from "@/utils/db";
 import { NextResponse } from "next/server";
+import connect from "@/utils/db";
+import Post from "@/models/Post";
 
 export const GET = async (request, { params }) => {
   const { id } = params;
@@ -12,7 +12,20 @@ export const GET = async (request, { params }) => {
 
     return new NextResponse(JSON.stringify(post), { status: 200 });
   } catch (err) {
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return new NextResponse("Database Error", { status: 500 });
   }
 };
 
+export const DELETE = async (request, { params }) => {
+  const { id } = params;
+
+  try {
+    await connect();
+
+    await Post.findByIdAndDelete(id);
+
+    return new NextResponse("Post has been deleted", { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
